59ad5e70a9801c1010aacc8fa162c76ffe86751b
Lindalab <linda.arthur@ashesi.edu.gh>
2022-12-07 18:13:16 +0000

    My name payment

--- view/checkout.php
+++ view/checkout.php
@@ -239,7 +239,7 @@ require_once("../functions/displayCart.php");
             let handler = PaystackPop.setup({
                 key: 'pk_test_1917da2fc21c61a25eb4edb146b723e98b2a7969', // Replace with your public key
                 // email: document.getElementById("email-address").value,
-                email: 'arthurlinda968@gmail.com',
+                email: '<?php echo $_SESSION['customer_email'];?>',
                 // amount: document.getElementById("amount").value * 100,
                 amount: <?php echo $catTotal ?> * 100,
                 ref: Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
