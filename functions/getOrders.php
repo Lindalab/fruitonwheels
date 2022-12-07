50342d290113583f89bdf03c98b3ded45be21f22
Lindalab <linda.arthur@ashesi.edu.gh>
2022-12-07 19:16:42 +0000

    Order table

--- functions/getOrders.php
+++ functions/getOrders.php
@@ -5,20 +5,21 @@ function orderedViewTable_fnc()
 {
     $data = showCustomerOrders_ctr();
     echo "
-    <table class='cart-table'>
-	<thead class='cart-table-head'>
-	<tr class='table-head-row'>
-    <th class='product-image'>Product Image</th>
-	<th class='product-quantity'>Quantity</th>
-	<th class='product-price'>Price</th>
-	<th class='customer-name'>Customer Name</th>
-    <th class='customer-contact'>Customer Contact</th>
-	<th class='customer-no'>Invoice No</th>
-	<th class='order-status'>Order Status</th>
-	<th class='product-total'>Total(GHC)</th>
-	</tr>
-	</thead>
-	<tbody>
+
+    <table class='table'>
+    <thead class='table-warning'>
+        <tr>
+        <th scope='col'>Product Image</th>
+          <th scope='col'> Quantity</th>
+          <th scope='col'>Price</th>
+          <th scope='col'>Customer Name</th>
+          <th scope='col'>Customer Contact</th>
+          <th scope='col'>Invoice No</th>
+          <th scope='col'>Total(GHC)</th>
+        </tr>
+      </thead>
+      <tbody>
+
     
     ";
 
@@ -29,10 +30,9 @@ function orderedViewTable_fnc()
         $customerName=$product['customer_name'];
         $customerContact=$product['customer_contact'];
         $invoiceNo=$product['invoice_no'];
-        $orderStatus=$product['order_status'];
         $total=$orderquantity*$price;
         
-        orderedItem_fnc( $productImage, $orderquantity,$price,$customerName,$customerContact,$invoiceNo,$orderStatus,$total);
+        orderedItem_fnc( $productImage, $orderquantity,$price,$customerName,$customerContact,$invoiceNo,$total);
     }
     echo "
     </tbody>
@@ -40,7 +40,7 @@ function orderedViewTable_fnc()
     ";
 }
 
-function orderedItem_fnc( $productImage, $orderquantity,$price,$customerName,$customerContact,$invoiceNo,$orderStatus,$total)
+function orderedItem_fnc( $productImage, $orderquantity,$price,$customerName,$customerContact,$invoiceNo,$total)
 {
     echo "
     <tr class='table-body-row'>
@@ -50,7 +50,6 @@ function orderedItem_fnc( $productImage, $orderquantity,$price,$customerName,$cu
     <td class='customer-name'>$customerName</td>
     <td class='customer-contact'>$customerContact</td>
 	<td class='customer-no'>$invoiceNo</td>
-    <td class='order-status'>$orderStatus</td>
 	<td class='product-total'>$total</td>
 	</tr>
     ";
